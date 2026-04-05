b08781434bb2f5e31557abf454c85640ca56cb2d
Yngdnmnc <158300674+Yngdnmnc@users.noreply.github.com>
2026-04-05 17:36:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gastotracker-v1';
+const CACHE_NAME = 'gastotracker-v2';
 const ASSETS = [
   '/',
   '/index.html',
@@ -6,6 +6,7 @@ const ASSETS = [
   '/js/app.js',
   '/js/store.js',
   '/js/sheets.js',
+  '/js/scanner.js',
   '/manifest.json'
 ];
 
